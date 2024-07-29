849c73d9d9535a87e2820d4f729654bdf569acee
RubenCaetano <72286933+RubenCaetano@users.noreply.github.com>
2024-07-29 14:08:06 +0100

    Create Web API project

---
+++ Contacts.WebApi/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Contacts.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.WebApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
